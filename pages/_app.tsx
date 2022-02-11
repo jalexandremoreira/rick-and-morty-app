088e58751a7405428bae96f4afacf355fed29b4c
Alexandre Moreira <brainmunchers@gmail.com>
2022-02-11 20:50:44 +0100

    bug fix 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import client from '../apollo-client';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
-import '../styles/globals.css';
+import client from '../apollo-client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
